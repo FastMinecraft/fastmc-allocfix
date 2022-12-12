963d677d5b579e60f6d8f40dc3d70435f346c4f7
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-12 18:33:42 -0500

    Replace inject with Overwrite to reduce the amount of CallbackInfoReturnable allocation

--- architectury-1.16.5/common/src/main/kotlin/dev/fastmc/allocfix/FastMcAllocFixEntryPoint.kt
+++ architectury-1.16.5/common/src/main/kotlin/dev/fastmc/allocfix/FastMcAllocFixEntryPoint.kt
@@ -3,6 +3,5 @@ package dev.fastmc.allocfix
 import net.fabricmc.api.ModInitializer
 
 class FastMcAllocFixEntryPoint : ModInitializer {
-    override fun onInitialize() {
-    }
+    override fun onInitialize() {}
 }
