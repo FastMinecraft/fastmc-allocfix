847a96b54255fbbca40bede921aeb850349db272
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-24 04:05:43 -0500

    WeightedBakedModel.getQuads() optimization

---
+++ architectury-1.18.2/common/src/main/java/dev/fastmc/allocfix/main/render/MixinWeightedBakedModel.java
@@ -0,0 +1,42 @@
+package dev.fastmc.allocfix.main.render;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.BakedQuad;
+import net.minecraft.client.render.model.WeightedBakedModel;
+import net.minecraft.util.collection.Weighted;
+import net.minecraft.util.math.Direction;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+@Mixin(WeightedBakedModel.class)
+public class MixinWeightedBakedModel {
+    @Shadow
+    @Final
+    private List<Weighted.Present<BakedModel>> models;
+
+    @Shadow
+    @Final
+    private int totalWeight;
+
+    /**
+     * @author Luna
+     * @reason Memory allocation optimization
+     */
+    @Overwrite
+    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction face, Random random) {
+        int weight = Math.abs((int)random.nextLong()) % this.totalWeight;
+        for (Weighted.Present<BakedModel> model : models) {
+            if ((weight -= model.getWeight().getValue()) >= 0) continue;
+            return model.getData().getQuads(state, face, random);
+        }
+        return Collections.emptyList();
+    }
+}
