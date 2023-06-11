3eceaacba45e1238e1e6aa22c7575709aecd756f
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-06-11 06:40:57 -0400

    Updated fastmc-common

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
-    java8ModCore("dev.fastmc:fastmc-common:1.0-SNAPSHOT:java8") {
+    java8ModCore("dev.fastmc:fastmc-common:1.1-SNAPSHOT:java8") {
         isTransitive = false
     }
-    java17ModCore("dev.fastmc:fastmc-common:1.0-SNAPSHOT:java17") {
-        isTransitive = false
-    }
+//    java17ModCore("dev.fastmc:fastmc-common:1.1-SNAPSHOT:java17") {
+//        isTransitive = false
+//    }
 }
